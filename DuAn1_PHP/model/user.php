df8774377ed737d2c7c5cb3017baa6b7e05255cb
unknown <mienvtph30480@fpt.edu.vn>
2023-08-03 08:36:33 +0700

    delete all file mien_admin

--- DuAn1_PHP/model/user.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    function checkout($username,$pass)
-    {
-        $sql = "SELECT * FROM `admin` WHERE username='".$username."' ";
-        $user = pdo_query_one($sql);
-        return $user;
-    }
-?>
