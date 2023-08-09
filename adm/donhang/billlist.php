faefc1d764c917fa820fb61cafd22c69146b2987
dungnv30284 <dungnvph30284@fpt.edu.vn>
2023-08-09 21:03:56 +0700

    change30

--- adm/donhang/billlist.php
+++ adm/donhang/billlist.php
@@ -26,39 +26,17 @@
     <link rel="stylesheet" href="../../css/style.css" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
+     <style>
+        th,tr,td{
+            border: 3px solid gray;
+        }
+     </style>
 </head>
 
 <body>
     <header class="header">
         <div class="container-fluid">
-            <!-- <div class="row">
-                <div class="col-xl-3 col-lg-2">
-                    <div class="header__logo">
-                        <a href="index.html"><img src="img/logo.png" alt=""></a>
-                    </div>
-                </div>
-                 <div class="col-xl-4 col-lg-5">
-                    <nav class="header__menu">
-                        <ul>
-                            <li class=""><a href="../../adm/index.php" class="text-decoration-none">Home</a></li>
-                            <li><a href="../sp/index.php" class="text-decoration-none">Sản phẩm</a></li>
-                            <li><a href="../danhmuc/index.php" class="text-decoration-none">Danh mục</a></li>
-                            <li><a href="../donhang/index.php" class="text-decoration-none">Đơn hàng</a></li>
-
-                        </ul>
-                    </nav>
-                </div>
-                <div class="col-xl-5">
-                    <div class="header__right">
-                        <div class="header__right__auth">
-
-                            < ?php require '../../login.php' ?>
-                        </div>
-
-                    </div>
-                </div>
-            </div> -->
+            
             <div class="canvas__open">
                 <i class="fa fa-bars"></i>
             </div>
@@ -80,106 +58,16 @@
     <section>
         <hr>
     </section>
-    <!-- <section class="shop-cart spad">
-        <div class="container mx-auto">
-            <div class="  row">
-
-                < ?php
-               
-                foreach ($s as $d) {
-
-
-
-
-                    ?>
-
-
-                    <div class="dropdown col-xl-3 my-5 text-center">
-                        <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
-                            < ?= $d['ma_hd'] ?>
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item bg-info text-white font-weight-bold text-uppercase" href="#">
-                                    Bill's Details:</a></li>
-                            <li><a class="dropdown-item" href="#"> In total: <span class="text-danger">
-                                        < ?php echo number_format($d['tong_tien'],0,',')  ?> VND
-                                    </span></a></li>
-                            <li><a class="dropdown-item" href="#">The amount: <span class="text-success">
-                                        < ?= $d['tongsl'] ?>
-                                    </span></a></li>
-                            <li><a class="dropdown-item font-weight-bold" href="#">Name:
-                                    < ?= $d['hoten'] ?>
-                                </a></li>
-                            <li><a class="dropdown-item font-weight-bold" href="#">Address:
-                                    < ?= $d['diachi'] ?>
-                                </a></li>
-                            <li><a class="dropdown-item font-weight-bold" href="#">Phone's number:
-                                    < ?= $d['sdt'] ?>
-                                </a></li>
-                            <li><a class="dropdown-item " href="#">Payment:
-                                    < ?php if ($d['payment'] == 1) {
-                                        echo "Paying when receiving!";
-                                    } elseif ($d['payment'] == 2) {
-                                        echo "Paying through Bank's account!";
-                                    } elseif ($d['payment'] == 3) {
-                                        echo "Paying through App(MOMO, ZaloPay...)!";
-                                    }
-                                    ?>
-                                </a></li>
-                            <li class="bg-warning"><a class="dropdown-item" href="#">Status:
-                                    < ?php
-                                    $ma_hd = $d['ma_hd'];
-                                    $t = status_select1($ma_hd);
-                                    if ($t['stt_id'] == 1) {
-                                        echo "On waiting!";
-                                    } elseif ($t['stt_id'] == 2) {
-                                        echo "On processing : We are preparing!";
-                                    } elseif ($t['stt_id'] == 3) {
-                                        echo "On processing: We have send to the shipping service!";
-                                    } elseif ($t['stt_id'] == 4) {
-                                        echo "Done: Successfully shipping!";
-                                    }
-
-                                    ?>
-
-                                </a></li>
-
-
-                        </ul>< ?php if(isset($t['stt_id'])){
-                            ?>
-                          <a class="btn btn-danger" href="index.php?act=updatestt&ma_hd=< ?= $d['ma_hd'] ?>" class="text-success">Update Bills' Status</a>
-< ?php
-                        }
-                        else{ ?>
-                        <a class="btn btn-warning" href="index.php?act=addstt&ma_hd=< ?= $d['ma_hd'] ?>" class="text-success">Add New Status</a>
-                            < ? php }?>
-                       
-                    </div>
-                < ?php } ?>'
-
-
-
-
-
-
-
-
-
-
-            </div>
-        </div>
-
-
-    </section> -->
+      
     <section class="shop-cart spad">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
                     <div class="shop__cart__table">
 
-                        <table class="text-center">
-                            <thead>
-                                <tr>
+                        <table class="text-center ">
+                            <thead   >
+                                <tr border="1">
 
                                     
                                     <th>Bill ID</th>
@@ -202,20 +90,20 @@
 
 
                    ?>
-                                    <tr>
+                                    <tr border="1">
                                        
-                                        <td class="font-weight-bold">
+                                        <td class="font-weight-bold" border="1">
                                             <?= $d['ma_hd'] ?>
                                         </td>
-                                        <td class="text-success font-weight-bold">
+                                        <td class="text-success font-weight-bold" border="1">
                                             <?= number_format($d['tong_tien'], 0, ',') ?>
 
                                         </td>
-                                        <td>
+                                        <td border="1">
                                             <?= $d['tongsl'] ?>
                                             <!-- < ?= $item[2] ?> -->
                                         </td>
-                                        <td colspan="2">
+                                        <td colspan="2" class="font-weight-bold" border="1">
                                             Họ tên:
                                             <?= $d['hoten'] ?> <br>
                                             Địa chỉ:
@@ -223,7 +111,7 @@
                                             SDT:
                                             <?= $d['sdt'] ?> <br>
                                         </td>
-                                        <td class="cart__price text-danger">
+                                        <td class="cart__price text-danger" border="1">
                                             <?php $payment = [1, 2, 3];
                                             if ($d['payment'] == $payment[0]) {
                                                 echo "Thanh toán khi nhận hàng!";
@@ -234,7 +122,7 @@
                                             }
                                             ?>
                                         </td>
-                                        <td>
+                                        <td class="text-primary font-weight-bold" border="1">
                                             <?php
                                             $stt = [1,2,3,4];
                                             $ma_hd = $d['ma_hd'];
@@ -256,7 +144,7 @@
                                             ?>
                                         </td>
 
-                                        <td >
+                                        <td border="1">
                                         <?php if(isset($t['stt_id'])){
                             ?>
                           <a class="btn btn-danger" href="index.php?act=updatestt&ma_hd=<?= $d['ma_hd'] ?>" class="text-success">Update Bills' Status</a>
