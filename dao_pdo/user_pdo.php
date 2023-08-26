0c151cedbf6b965628b01596b8acc443a5d1ef83
dungnv30284 <dungnvph30284@fpt.edu.vn>
2023-08-26 22:18:09 +0700

    adm_user

--- dao_pdo/user_pdo.php
+++ dao_pdo/user_pdo.php
@@ -9,3 +9,15 @@ function user_register($username,$user_mail,$user_phone,$user_address,$user_pass
     $sql = "insert into users(username,user_mail,user_phone,user_address,user_password) values (?,?,?,?,?)";
     pdo_execute($sql,$username,$user_mail,$user_phone,$user_address,$user_password);
 }
+function user_select_all(){
+    $sql = "select * from users ";
+    return pdo_query($sql);
+}
+function user_bill($username){
+    $sql = "select count(ma_hd) from bills where hoten = ?";
+    return pdo_query_one($sql,$username);
+}
+function user_update($user_mail,$user_phone,$user_address,$username){
+    $sql = "update users set user_mail = ?,user_phone = ?,user_address = ? where username = ?";
+    pdo_execute($sql,$user_mail,$user_phone,$user_address,$username);
+}
